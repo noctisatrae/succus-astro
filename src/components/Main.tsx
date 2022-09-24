f409cb77d8a7d7f8b329aa847a9d2d427dac564e
noctisatrae <pro.hector.kub@gmail.com>
2022-09-24 20:01:17 +0200

    Prototype

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,19 +2,17 @@
 import React, { useState, useEffect } from "react";
 import TimeAgo from 'javascript-time-ago'
 
-import { connectWallet, sendmessage, receiveMessageConstant, dbConf } from "succus";
-
-import { SEA } from "db";
+import { sendmessage, receiveMessage, SEA, getKeypair, registerKeypair } from "succus";
 
 // English.
 import en from 'javascript-time-ago/locale/en'
 
 TimeAgo.addDefaultLocale(en)
-
 const timeAgo = new TimeAgo('en-US')
+
 const gunKeypair = {pub: '24sFqelMjISareVwxjBzEis1GsVyBL82P1sxUbLB_JI.wGBPSof3EwGHw6ITReATTUco0INz5-qFZA_TFEV5i9U', priv: 'UOOT4jK0V-5G7HaOWCEKy529rEmWjxe9BnUewG55a7o', epub: 'GhjvQH2DcJip7kIhvrcGZPQBv7gSSZ7aLJOG96anZ10.u-oN-R7p8xmbiBjSyodssX9yaJTOGPEWnfQrSuBZNZw', epriv: 'gaKPOm_Wlv7GLB36iNcBAOr8TIGU17qtw71bYej6pk8'}
 
-await dbConf({ radisk: false, peers:["http://localhost:4397/gun", "https://gun-manhattan.herokuapp.com/gun"] })
+const address = "0x84673f99d9807780ce5Db4c3A980d708535d9604"
 
 const Main = () => {
 
@@ -23,9 +21,7 @@ const Main = () => {
 
     const updateMsg = async () => {
 
-        const {address} = await connectWallet();
-        await receiveMessageConstant([address], async ({encryptedMSG, date, from, ensFrom}) => {
-
+        await receiveMessage([address], async ({encryptedMSG, date, from, ensFrom}) => {
 
             console.log(encryptedMSG)
 
@@ -37,8 +33,20 @@ const Main = () => {
     }
 
     useEffect(() => {
-        updateMsg()
+
+        (async () => {
+            await registerKeypair(address, gunKeypair);
+
+            const {epub, pub} = await getKeypair(address)
+            
+            console.log({epub, pub})
+        })();
+
     }, [])
+
+    useEffect(() => {
+        updateMsg()
+    }, [setMessages])
 
     return <div>
 
@@ -48,10 +56,7 @@ const Main = () => {
 
         <p>{msgInput}</p>
         <button class="btn btn-secondary" onClick={async () => {
-            
-            const {address} = await connectWallet();
-
-            await sendmessage(msgInput, [address], gunKeypair);
+            console.log("[output]: ", await sendmessage(msgInput, [address], gunKeypair));
         }}>Send</button>
 
         {messages.map(({content, name, from, sentAt}) => (
